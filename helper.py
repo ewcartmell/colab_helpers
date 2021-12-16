85de024d8420d2e1b97d75a784955778ad904ce7
ewcartmell <ewcartmell@gmail.com>
2021-12-15 19:54:01 -0800

    One more try

--- helper.py
+++ helper.py
@@ -73,7 +73,7 @@ def blended_rate(output_df, rates_df, site_keyword = 'site', rate_keyword = 'mon
 
 def update_transactions_per_hc(df, grouping_column = ['team','site','worker_type'], date_column = 'month', cutoff_date = '2022-01-01'):
   temp_df = df.copy()
-  sort_by = grouping_column.append(date_column)
+  sort_by = grouping_column + [date_column]
   temp_df.sort_values(sort_by)
 
   cpi = 'cumulative_productivity_improvement'
